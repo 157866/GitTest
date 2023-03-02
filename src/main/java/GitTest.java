ca3638e6b3377bf35803abbee595cf13c7870ca0
157866 <2849569455@qq.com>
2023-03-02 11:18:29 +0800

    Commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * @author wmt
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2023/3/211:17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
